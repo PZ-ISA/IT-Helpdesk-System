a93bc0ab6c5148a8d0822579157daa91881d0ae6
Aleksander <alek@zolnowski.eu>
2025-05-17 14:52:24 +0200

    RefreshToken update DateTime to DateTimeOffSet

---
+++ src/HelpdeskSystem.Infrastructure/Migrations/20250517124004_UpdateRefreshToken.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HelpdeskSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRefreshToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isRevoked",
+                table: "RefreshTokens",
+                newName: "IsRevoked");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ExpiresAt",
+                table: "RefreshTokens",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsRevoked",
+                table: "RefreshTokens",
+                newName: "isRevoked");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ExpiresAt",
+                table: "RefreshTokens",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+        }
+    }
+}
